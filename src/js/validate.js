16c4734c61a8bfc59a9042a8b99c50c5fcb62c1d
tashakibanova <ov.nattie@gmail.com>
2024-05-15 11:53:24 +0300

    change validate.js

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,20 +1,24 @@
 export default function luhnAlgoritm(value) {
-  let sum = 0;
+  let nCheck = 0;
+  let nLength = value.length;
+  let isSecond = false;
 
-  for (let i = 0; i < value.length; i++) {
-    let digit = parseInt(value[value.length - 1 - i], 10);
+  for (let i = nLength - 1; i >= 0; i--) {
+    
+    let nDigit = parseInt(value[i], 10);
+    
+    if (isSecond == true)
+        nDigit *= 2;
 
-    if (i % 2 !== 0) {
-      digit *= 2;
+    // Restamos 9 de los dígitos
+    if (nDigit > 9)
+        nDigit -= 9;
 
-      if (digit > 9) {
-        digit -= 9;
-      }
-    }
-    sum += digit;
+    nCheck += nDigit;
+    isSecond = !isSecond;
   }
 
-  return sum % 10 === 0;
+  return (nCheck % 10 === 0);
 }
 
 
