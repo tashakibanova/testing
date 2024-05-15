33eeb44b1def2955621488e45a18abb6b8197244
tashakibanova <ov.nattie@gmail.com>
2024-05-15 11:25:53 +0300

    change validate.js

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,15 +1,14 @@
 export default function luhnAlgoritm(setValue) {
-  
-  let nCheck = 0;
-  let nDigit = 0;
-  let bEven = false;
+  let nCheck = 0, bEven = false;
   setValue = setValue.replace(/\D/g, "");
 
   for (let n = setValue.length - 1; n >= 0; n--) {
-      let cDigit = setValue.charAt(n),
+      var cDigit = setValue.charAt(n),
             nDigit = parseInt(cDigit, 10);
 
-      if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+      if (bEven) {
+          if ((nDigit *= 2) > 9) nDigit -= 9;
+      }
 
       nCheck += nDigit;
       bEven = !bEven;
